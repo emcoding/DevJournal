a9d6fea96dd7431dcc12d728aa376cf3f77d4c0a
F3PiX <F3PiXnl@gmail.com>
2016-08-08 17:10:28 +0200

    Redirect create and update to index view

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -28,7 +28,7 @@ class SnippetsController < ApplicationController
 
     respond_to do |format|
       if @snippet.save
-        format.html { redirect_to @snippet, notice: 'Snippet was successfully created.' }
+        format.html { redirect_to action: :index, notice: 'Snippet was successfully created.' }
         format.json { render :show, status: :created, location: @snippet }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class SnippetsController < ApplicationController
   def update
     respond_to do |format|
       if @snippet.update(snippet_params)
-        format.html { redirect_to @snippet, notice: 'Snippet was successfully updated.' }
+        format.html { redirect_to action: :index, notice: 'Snippet was successfully updated.' }
         format.json { render :show, status: :ok, location: @snippet }
       else
         format.html { render :edit }
