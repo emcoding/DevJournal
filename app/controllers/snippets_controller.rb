f5124b1a9cfcd8a4c4def9a212ba7461b4a0c270
F3PiX <F3PiXnl@gmail.com>
2016-08-08 18:31:20 +0200

    Add js to show form in card on index page

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -28,7 +28,7 @@ class SnippetsController < ApplicationController
 
     respond_to do |format|
       if @snippet.save
-        format.html { redirect_to action: :index, notice: 'Snippet was successfully created.' }
+        format.html { redirect_to snippets_path, notice: 'Snippet was successfully created.' }
         format.json { render :show, status: :created, location: @snippet }
       else
         format.html { render :new }
