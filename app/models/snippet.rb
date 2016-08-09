8a525f6626bdaa80b3d438346753d6fe9bc1468f
F3PiX <F3PiXnl@gmail.com>
2016-08-09 19:05:54 +0200

    Extend search to title and content

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -4,7 +4,8 @@ class Snippet < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   def self.search(search)
-    where("title LIKE ?", "%#{search}%")
+    where("title LIKE ? OR content LIKE ?", "%#{search}%", "%#{search}%")
+    #where("content LIKE ? OR name LIKE ?", "%#{search}%")
   end
 
 end
