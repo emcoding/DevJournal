941c46aa2712e62a9004e7c7b173e5f57b82d51a
F3PiX <F3PiXnl@gmail.com>
2016-08-09 19:03:51 +0200

    Add search

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -2,4 +2,9 @@ class Snippet < ApplicationRecord
   validates :title, presence: true
 
   mount_uploader :image, ImageUploader
+
+  def self.search(search)
+    where("title LIKE ?", "%#{search}%")
+  end
+
 end
