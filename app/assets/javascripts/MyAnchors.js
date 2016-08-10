d242ca8d203a7d44ed1100d12770e7c0786a253d
F3PiX <F3PiXnl@gmail.com>
2016-08-10 23:44:19 +0200

    Add anchors to snippet titles

---
+++ app/assets/javascripts/MyAnchors.js
@@ -0,0 +1,10 @@
+/**
+ * Created by maud on 10-8-16.
+ */
+
+$(document).ready(function() {
+    anchors.options = {
+        placement: 'right'
+    }
+    anchors.add('#sniptitle h4');
+});
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,5 @@
 //= require bootstrap
 //= require jquery_ujs
 //= require turbolinks
+//= require anchor
 //= require_tree .
