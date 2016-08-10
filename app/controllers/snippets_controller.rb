c7e3de21f2354bd652cb94116bf74282402ca3a8
F3PiX <F3PiXnl@gmail.com>
2016-08-10 09:59:18 +0200

    Add 'remove image' checkbox to form

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -73,6 +73,6 @@ class SnippetsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def snippet_params
-      params.require(:snippet).permit(:title, :content, :image)
+      params.require(:snippet).permit(:title, :content, :image, :remove_image)
     end
 end
