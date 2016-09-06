6f876ce1d4cb7ff7882eb2fbea83f2b68f7447fa
F3PiX <F3PiXnl@gmail.com>
2016-09-06 17:07:02 +0200

    Complete Moving to postgres
    After bundling. Belongs to d659f

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160904183855) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accounts", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -25,12 +28,9 @@ ActiveRecord::Schema.define(version: 20160904183855) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_accounts_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_accounts_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true, using: :btree
   end
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
 
   create_table "snippets", force: :cascade do |t|
     t.string   "title"
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 20160904183855) do
     t.datetime "updated_at", null: false
     t.string   "image"
     t.integer  "account_id"
-    t.index ["account_id"], name: "index_snippets_on_account_id"
+    t.index ["account_id"], name: "index_snippets_on_account_id", using: :btree
   end
 
   create_table "taggings", force: :cascade do |t|
@@ -67,4 +67,5 @@ ActiveRecord::Schema.define(version: 20160904183855) do
     t.index ["name"], name: "index_tags_on_name", unique: true, using: :btree
   end
 
+  add_foreign_key "snippets", "accounts"
 end
