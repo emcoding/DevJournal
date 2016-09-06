a3375c1a02e23fdafed06bb27596e4b0bccc44d8
F3PiX <F3PiXnl@gmail.com>
2016-09-06 15:37:39 +0200

    Add Devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812101245) do
+ActiveRecord::Schema.define(version: 20160904183855) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_accounts_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
 
   create_table "snippets", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +35,8 @@ ActiveRecord::Schema.define(version: 20160812101245) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "image"
+    t.integer  "account_id"
+    t.index ["account_id"], name: "index_snippets_on_account_id"
   end
 
   create_table "taggings", force: :cascade do |t|
