ed8473e312682e3263edcc9ff870178e8d1e3b32
Maud de Vries <F3PiX@users.noreply.github.com>
2016-11-04 22:48:13 +0100

    Improve click behaviour in snippet body (#69)

--- app/assets/javascripts/snippets.js
+++ app/assets/javascripts/snippets.js
@@ -2,18 +2,16 @@
  * Created by maud on 03-9-16.
  */
 
+$(document).on('click', '.snippet-card-clickable', function(event) {
+    window.location = this.getAttribute("data-snippet-url");
+});
+
+$(document).on('click', '.snippet-card-clickable a', function(event) {
+    event.stopPropagation();
+});
+
 $(document).on('turbolinks:load', function() {
-
-    //open links on new tab
     $( 'a[href^="http"]' ).attr( 'target','_blank' );
-
-    //click within card area to edit snippet
-    $('.snippet-card').on('click', function(event) {
-        if ( !$(event.target).closest('form').length ) {
-
-            window.location = this.getAttribute("data-snippet-url");
-        }
-    });
 });
 
 
